ce10c9588c6122f92c05cecd8ee3926234a924c7
GaspardPcht <gaspardpauchet@gmail.com>
2024-11-18 19:47:29 +0100

    Ajout de la balise meta viewport pour améliorer la réactivité de la mise en page

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/assets/withmac.png" type="image/png" />
       </head>
       <body>
